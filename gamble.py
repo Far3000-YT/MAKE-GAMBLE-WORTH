415d2f67aef868f5cb271e2f7bc950628566b8ed
Far3000-YT <far3000yt@gmail.com>
2024-11-29 16:10:14 +0100

    final

--- gamble.py
+++ gamble.py
@@ -1,8 +1,6 @@
-import sys
+from sys import stdout
 from random import randint
 from json import load
-
-import time
 
 with open("values.json", 'r+') as file:
     data = load(file)
@@ -10,19 +8,19 @@ with open("values.json", 'r+') as file:
 balance, bet_amount = int(data['balance']), int(data['bet_amount'])
 bet_true, i = True, 0
 
-#Reduced output frequency (for example, print every 100th bet)
+#reduced output frequency (print every 1k th iteration)
 while bet_true:
     i += 1
-    choice = randint(0, 1)  #0 for loss and 1 for win
-    
+    choice, freq = randint(0, 1), 1000 #0 for loss and 1 for win
     if choice == 1:
         balance += bet_amount
         bet_amount = data['bet_amount']
-        if i % 1000 == 0:
+        if i % freq == 0:
             print(f"WIN  - {i} - {choice} - {balance}")
     else:
         balance -= bet_amount
         bet_amount = bet_amount * 2
-        if i % 1000 == 0:
+        if i % freq == 0:
             print(f"LOSS - {i} - {choice} - {balance}")
-    sys.stdout.flush()
+        if balance <= 0: print("loss xd")
+    stdout.flush()
